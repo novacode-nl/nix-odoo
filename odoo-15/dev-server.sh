4ad9db864b59071ef897875d0334bc1cbe4db7ed
bobslee <bob@novacode.nl>
2022-05-16 10:09:03 +0200

    Fix dev-server.sh: test command

--- odoo-15/dev-server.sh
+++ odoo-15/dev-server.sh
@@ -148,7 +148,7 @@ case "$COMMAND" in
         ;;
     test)
         command_postgres &
-	.venv/bin/python ./odoo/odoo-bin -c odoo.conf -d $1 --test-enable --stop-after-init -i ${@:2}
+	.venv/bin/python ./odoo/odoo-bin -c odoo.conf -d $1 --test-enable --stop-after-init -i "${@: 2}"
         ;;
     postgres)
         command_postgres "$@"
