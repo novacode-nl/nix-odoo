f41f12f3484d603cd880e79b5753658a7d8d14ce
bobslee <bob@novacode.nl>
2022-09-20 12:15:40 +0200

    AFTER_RESTORE.sql: ir_config_parameter 'auth_totp.policy'

--- odoo-15/AFTER_RESTORE.sql
+++ odoo-15/AFTER_RESTORE.sql
@@ -3,4 +3,5 @@ DELETE FROM ir_mail_server;
 UPDATE ir_cron SET active = False;
 UPDATE ir_config_parameter SET VALUE = 'accept' WHERE KEY = 'server.mode';
 UPDATE ir_config_parameter SET VALUE = 'https://example.com/api' WHERE KEY = 'EXAMPLE.api.url';
+UPDATE ir_config_parameter SET VALUE = 'False' WHERE KEY = 'auth_totp.policy';
 UPDATE res_users SET login='admin', password = 'admin' WHERE id = 2;
