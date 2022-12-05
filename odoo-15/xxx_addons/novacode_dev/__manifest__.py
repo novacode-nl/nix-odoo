11ca386cbe1134b426508261c3416410173fd146
bobslee <bob@novacode.nl>
2022-12-05 16:51:03 +0100

    Add server action: Reset formio.js version

--- odoo-15/xxx_addons/novacode_dev/__manifest__.py
+++ odoo-15/xxx_addons/novacode_dev/__manifest__.py
@@ -4,12 +4,14 @@
 {
     'name': 'Nova Code | Dev addons',
     'summary': 'Nova Code | Dev addons',
-    'version': '14.0.1.0.0',
+    'version': '14.0.1.0.1',
     'license': 'LGPL-3',
     'author': 'Nova Code',
     'website': 'https://www.novacode.nl',
     'category': 'Extra Tools',
     'depends': ['base'],
-    'data': [],
+    'data': [
+        'data/ir_actions_server.xml'
+    ],
     'application': False,
 }
