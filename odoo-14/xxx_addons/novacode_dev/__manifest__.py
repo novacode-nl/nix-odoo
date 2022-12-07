111252d2d075c9e747e42e8838d3786a639cd940
bobslee <bob@novacode.nl>
2022-12-07 18:12:26 +0100

    Unobtrusive server action: Reset formio.js version

--- odoo-14/xxx_addons/novacode_dev/__manifest__.py
+++ odoo-14/xxx_addons/novacode_dev/__manifest__.py
@@ -4,7 +4,7 @@
 {
     'name': 'Nova Code | Dev addons',
     'summary': 'Nova Code | Dev addons',
-    'version': '14.0.1.0.1',
+    'version': '14.0.1.0.2',
     'license': 'LGPL-3',
     'author': 'Nova Code',
     'website': 'https://www.novacode.nl',
