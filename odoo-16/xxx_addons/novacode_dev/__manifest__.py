cf53c84503aaa6361a0eb8f23bb8abf5c9f242cf
bobslee <bob@novacode.nl>
2023-05-11 13:17:24 +0200

    Update novacode_dev/__manifest__.py

--- odoo-16/xxx_addons/novacode_dev/__manifest__.py
+++ odoo-16/xxx_addons/novacode_dev/__manifest__.py
@@ -2,9 +2,9 @@
 # See LICENSE file for full licensing details.
 
 {
-    'name': 'Nova Code | Dev addons',
-    'summary': 'Nova Code | Dev addons',
-    'version': '14.0.1.0.2',
+    'name': 'Nova Code | Dev Tools',
+    'summary': 'Nova Code | Dev Tools',
+    'version': '16.0.1.0.2',
     'license': 'LGPL-3',
     'author': 'Nova Code',
     'website': 'https://www.novacode.nl',
