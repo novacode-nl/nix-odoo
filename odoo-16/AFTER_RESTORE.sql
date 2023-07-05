52129cffdfe1f04e3575bba82660e84c3e5c1f9b
bobslee <bob@novacode.nl>
2023-07-05 15:30:22 +0200

    Update AFTER_RESTORE.sql
    -- for all users
    UPDATE res_users SET totp_secret = NULL;

--- odoo-16/AFTER_RESTORE.sql
+++ odoo-16/AFTER_RESTORE.sql
@@ -6,7 +6,8 @@ DELETE FROM ir_config_parameter WHERE key = 'auth_totp.policy';
 DELETE FROM account_online_link;
 DELETE FROM printnode_printer;
 
-UPDATE res_users SET login='admin', password = 'admin', totp_secret = NULL WHERE id = 2;
+UPDATE res_users SET login='admin', password = 'admin' WHERE id = 2;
+UPDATE res_users SET totp_secret = NULL;
 
 UPDATE ir_cron SET active = False;
 UPDATE ir_config_parameter SET VALUE = 'accept' WHERE KEY = 'server.mode';
